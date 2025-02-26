532c6943fd88a3281d145d23106b76b1e0db59ef
Yogesh <yogesh.gorrepati30@gmail.com>
2025-02-27 01:08:37 +0530

    Set Up Database and modeled prisma schema

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,8 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div>
-      <SignedOut>
-            <SignInButton mode="modal">
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"> Sign In </button>
-            </SignInButton>
-      </SignedOut>
-      <SignedIn>
-            <UserButton />
-      </SignedIn>
+    <div className="m-4">
+      Home
     </div>
   );
 }
