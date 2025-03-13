24aef21b827ad26d8773676a64b2550701d4b1f7
Yogesh <yogesh.gorrepati30@gmail.com>
2025-03-13 21:45:56 +0530

    Fixed Build Errors

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -1,40 +1,48 @@
 import {
-    getProfileByUsername,
-    getUserLikedPosts,
-    getUserPosts,
-    isFollowing,
-  } from "@/actions/profile.action";
-  import { notFound } from "next/navigation";
+  getProfileByUsername,
+  getUserLikedPosts,
+  getUserPosts,
+  isFollowing,
+} from "@/actions/profile.action";
+import { notFound } from "next/navigation";
 import ProfilePageClient from "./ProfilePageClient";
-  
-  export async function generateMetadata({ params }: { params: { username: string } }) {
-    const user = await getProfileByUsername(params.username);
-    if (!user) return;
-  
-    return {
+import { Metadata } from "next";
+
+type Props = {
+  params: { username: string }
+  searchParams: { [key: string]: string | string[] | undefined }
+}
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const user = await getProfileByUsername(params.username);
+  if (!user) return {
+      title: 'User not found',
+      description: 'This user does not exist'
+  };
+
+  return {
       title: `${user.name ?? user.username}`,
       description: user.bio || `Check out ${user.username}'s profile.`,
-    };
-  }
-  
-  async function ProfilePageServer({ params }: { params: { username: string } }) {
-    const user = await getProfileByUsername(params.username);
-  
-    if (!user) notFound();
-  
-    const [posts, likedPosts, isCurrentUserFollowing] = await Promise.all([
+  };
+}
+
+export default async function ProfilePage({ params }: Props) {
+  const user = await getProfileByUsername(params.username);
+
+  if (!user) notFound();
+
+  const [posts, likedPosts, isCurrentUserFollowing] = await Promise.all([
       getUserPosts(user.id),
       getUserLikedPosts(user.id),
       isFollowing(user.id),
-    ]);
-  
-    return (
+  ]);
+
+  return (
       <ProfilePageClient
-        user={user}
-        posts={posts}
-        likedPosts={likedPosts}
-        isFollowing={isCurrentUserFollowing}
+          user={user}
+          posts={posts}
+          likedPosts={likedPosts}
+          isFollowing={isCurrentUserFollowing}
       />
-    );
-  }
-  export default ProfilePageServer;
+  );
+}
