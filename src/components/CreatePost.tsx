e6d10e4bf3761f85254648442f6d2735fb727424
Yogesh <yogesh.gorrepati30@gmail.com>
2025-03-13 21:36:44 +0530

    Added Photo Upload Feature

--- src/components/CreatePost.tsx
+++ src/components/CreatePost.tsx
@@ -10,6 +10,7 @@ import { Button } from './ui/button';
 import { ImageIcon, Loader2Icon, SendIcon } from 'lucide-react';
 import { createPost } from '@/actions/post.action';
 import toast from 'react-hot-toast';
+import ImageUpload from './ImageUpload';
 
 function CreatePost() {
   const {user}= useUser();
@@ -55,6 +56,18 @@ function CreatePost() {
           </div>
 
           {/*TODO: Add image upload*/}
+          {(showImageUpload || imageUrl) && (
+            <div className="border rounded-lg p-4">
+              <ImageUpload
+                endpoint="postImage"
+                value={imageUrl}
+                onChange={(url) => {
+                  setImageUrl(url);
+                  if (!url) setShowImageUpload(false);
+                }}
+              />
+            </div>
+          )}
           
 
           <div className="flex items-center justify-between border-t pt-4">
